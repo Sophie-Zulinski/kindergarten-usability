fdd0d419d3d42aa144a65c5640c386830db97313
sophokel <s_hurricane@gmx.net>
2023-05-18 09:59:47 +0200

    remove unneccessary files and add new kindergartens

--- src/hooks/use-kindergartens-context.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import KindergartenContext from "../context/kindergartens";
-
-function useKindergartensContext() {
-  return useContext(KindergartenContext);
-}
-
-export default useKindergartensContext;
